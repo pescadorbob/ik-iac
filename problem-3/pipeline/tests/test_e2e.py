adc7f753d46ba4dd79733e76aa11fd7137586259
Brent Fisher <bc.fisher@yahoo.com>
2024-06-30 15:39:40 -0700

    E2E test created and run locally for validator

---
+++ problem-3/pipeline/tests/test_e2e.py
@@ -0,0 +1,32 @@
+import pytest
+from ..deploy.deployment_validator import DeploymentValidator
+from ..deploy.info_gateway import DeploymentInfoResponse, InfoGateway
+from ..deploy.clock import Clock
+from ..deploy.deployment_validator_configuration import DeploymentValidatorConfiguration
+from ..deploy.environment import Environment
+from datetime import datetime, timedelta
+
+class TestEndToEnd():
+
+    def live_deployment_info(self) -> str:
+        return "2024-06-30T22:34:58.984Z"
+
+    def getEnvironment(self) -> Environment:
+        return Environment("local","http://localhost:8080/actuator/info")
+        
+    
+
+    @pytest.fixture
+    def create_validator(self):
+        environment = self.getEnvironment()
+        configuration = DeploymentValidatorConfiguration.from_environment(environment)
+        self.use_case = DeploymentValidator(configuration)
+
+    def test_deployment_successful(self,create_validator):
+        target_build_info_time_of_deployment = self.live_deployment_info()
+        time_limit = timedelta(minutes=1)
+        retry_interval = timedelta(seconds=5)
+        isDeployed = self.use_case.validate(target_build_info_time_of_deployment,
+                          time_limit,
+                          retry_interval)
+        assert isDeployed == True
