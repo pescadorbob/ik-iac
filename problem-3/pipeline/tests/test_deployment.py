0c79c27a2663d3130343cca6904b708172192e7c
Brent Fisher <bc.fisher@yahoo.com>
2024-07-02 17:07:46 -0700

    abstracted deployment!

---
+++ problem-3/pipeline/tests/test_deployment.py
@@ -0,0 +1,23 @@
+
+import pytest
+from ..deploy.local_deployment import LocalDeployment
+from ..deploy.aws_deployment import AwsDeployment
+from abc import ABC, abstractmethod
+class TestDeployment(ABC):
+
+    @abstractmethod
+    def deployment(self):
+        pass
+
+
+    def test_deployment(self):
+        assert self.deployment().run_pipeline()
+        
+        
+class TestLocalDeployment(TestDeployment):
+    def deployment(self):
+        return LocalDeployment(".")
+    
+class TestAwsDeployment(TestDeployment):
+    def deployment(self):
+        return AwsDeployment(".")
